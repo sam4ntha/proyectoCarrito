1111035bd7384bfb022116c0758afc5ec4e1f75c
Samantha 3B <stap0428@gmail.com>
2022-10-03 11:07:29 -0500

    Recuperando datos de objetos seleccionados

--- js/app.js
+++ js/app.js
@@ -4,12 +4,26 @@ const listaCursos = document.querySelector('#lista-cursos');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 
+let listadoCarrito = [];
 
 const agregarCurso = (e) => {
     e.preventDefault();
     if(e.target.classList.contains('agregar-carrito')){
-        console.log("Presionaste el botón");
+        const curso = e.target.parentElement.parentElement;
+        const infoCurso = {
+            imagen: curso.querySelector('img').src,
+            nombre: curso.querySelector('h4').textContent,
+            precio: curso.querySelector('p.precio').textContent,
+            id: curso.querySelector('.agregar-carrito').getAttribute('data-id'),
+            cantidad: 1
+        }
+        
+        agregarCarrito(infoCurso);
     }
+}
+const agregarCarrito = curso => {
+    listadoCarrito = [...listadoCarrito, curso]
+    console.log(listadoCarrito);
 }
 
 const cargarEventListener = () => {
